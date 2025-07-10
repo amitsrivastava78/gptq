e85620d2e48fb170a79abda5084445af44ff09a4
Amit Srivastava <amitsrivasta@google.com>
2025-07-10 14:05:08 +0530

    Trying to fix tf add_batch in gptqkeras.py part 9

--- gptqkeras.py
+++ gptqkeras.py
@@ -27,7 +27,7 @@ class GPTQ:
         # Note: No Conv1D equivalent in Keras, so we skip that check
         self.rows = int(W.shape[0])
         self.columns = int(W.shape[1])
-        self.H = tf.zeros((self.columns, self.columns), dtype=tf.float32)
+        self.H = tf.zeros((self.rows, self.rows), dtype=tf.float32)
         self.nsamples = 0
         self.quantizer = None
 
